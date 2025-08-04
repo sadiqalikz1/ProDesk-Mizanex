dba6d9d7d0614236dde1e441a703f6b2fa5118e4
SADIQ ALI <sadiqaliksa1324@gmail.com>
2025-08-04 10:20:44 +0000

    same isue still solve using another methods

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,12 +13,12 @@ import {
 export default function Home() {
   return (
     <SidebarProvider>
-      <div className="flex min-h-screen w-full flex-col bg-muted/40">
+      <div className="relative flex min-h-screen w-full">
         <Sidebar>
           <AppSidebar />
         </Sidebar>
-        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-          <SidebarInset>
+        <SidebarInset>
+          <div className="flex flex-1 flex-col">
             <Header />
             <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
               <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
@@ -30,8 +30,8 @@ export default function Home() {
               </div>
               <QuickView />
             </main>
-          </SidebarInset>
-        </div>
+          </div>
+        </SidebarInset>
       </div>
     </SidebarProvider>
   );
