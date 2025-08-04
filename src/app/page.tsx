2ef63aeb5dd8b79e42117e1d94152e99fdf0854a
SADIQ ALI <sadiqaliksa1324@gmail.com>
2025-08-04 10:39:11 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <main class="grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,12 +18,16 @@ export default function Home() {
         </Sidebar>
         <SidebarInset>
           <Header />
-          <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
-            <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
-              <NotePad />
-              <TaskManager />
+          <main className="flex flex-1 flex-col gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+            <div className="flex flex-col gap-4 md:gap-8 lg:flex-row">
+              <div className="flex flex-col gap-4 md:gap-8 lg:w-2/3">
+                <NotePad />
+                <TaskManager />
+              </div>
+              <div className="lg:w-1/3">
+                <QuickView />
+              </div>
             </div>
-            <QuickView />
           </main>
         </SidebarInset>
       </div>
