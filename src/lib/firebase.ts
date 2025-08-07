da76883bdc814ed2955afdd72658b2869c2da069
SADIQ ALI <sadiqaliksa1324@gmail.com>
2025-08-07 07:29:38 +0000

    CONNECT REAL TIME DATABE https://prodesk-mizanex-default-rtdb.firebaseio

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,14 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApp, getApps } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
   authDomain: "prodesk-mizanex.firebaseapp.com",
+  databaseURL: "https://prodesk-mizanex-default-rtdb.firebaseio.com/",
   projectId: "prodesk-mizanex",
   storageBucket: "prodesk-mizanex.appspot.com",
   messagingSenderId: "99359146536",
@@ -20,10 +22,11 @@ const firebaseConfig = {
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const firestore = getFirestore(app);
 const auth = getAuth(app);
+const database = getDatabase(app);
 
 let analytics;
 if (typeof window !== 'undefined') {
   analytics = getAnalytics(app);
 }
 
-export { app, firestore, auth, analytics };
+export { app, firestore, auth, database, analytics };
