e628dd0ebf0e1ee873c37351395f7544bdf14854
SADIQ ALI <sadiqaliksa1324@gmail.com>
2025-08-07 11:11:38 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Card>
    
    with the t

--- src/app/physical-file-tracker/page.tsx
+++ src/app/physical-file-tracker/page.tsx
@@ -28,18 +28,20 @@ export default function PhysicalFileTrackerPage() {
                 <CreateFile />
                 <QuickAddToFile />
               </div>
-              <Tabs defaultValue="file-list">
-                <TabsList>
-                  <TabsTrigger value="file-list">File List</TabsTrigger>
-                  <TabsTrigger value="history-log">History Log</TabsTrigger>
-                </TabsList>
-                <TabsContent value="file-list">
-                  <PhysicalFileTracker />
-                </TabsContent>
-                <TabsContent value="history-log">
-                  <FileLog />
-                </TabsContent>
-              </Tabs>
+              <div className="w-11/12 mx-auto">
+                <Tabs defaultValue="file-list">
+                  <TabsList>
+                    <TabsTrigger value="file-list">File List</TabsTrigger>
+                    <TabsTrigger value="history-log">History Log</TabsTrigger>
+                  </TabsList>
+                  <TabsContent value="file-list">
+                    <PhysicalFileTracker />
+                  </TabsContent>
+                  <TabsContent value="history-log">
+                    <FileLog />
+                  </TabsContent>
+                </Tabs>
+              </div>
             </div>
           </main>
         </SidebarInset>
